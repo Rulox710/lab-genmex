fe7e65bf2975a3937c111e642e53486a0ce05300
Raúl Nunova <rnunovaldes@ciencias.unam.mx>
2026-09-15 13:38:36 -0600

    Agregando algunas funciones extras [NUEVAS] - Ahora se puede pedir operaciones de 2 o 3 números simultaneos con una misma operación [MANTENIMIENTO] - Se usan funciones lambda para las operaciones - Un bucle para pedir los números [PENDIENTE] - Permitir mezclar operaciones y permitir cuantos números quiera - Manejar la consola para que salga más limpio(?

--- pf-l4-main/main.py
+++ pf-l4-main/main.py
@@ -1,4 +1,4 @@
-ANSWERS: dict[str, str] = {
+ANSWERS = {
   'INV': '¡¡¡Lo que ingresó es inválido!!! ({:s})',
   'ENTER': "<ENTER para continuar>",
   'OPTION': "Seleccione una opción",
@@ -8,8 +8,17 @@ ANSWERS: dict[str, str] = {
   'NO': "No",
 }
 
-print('¿Qué quiere hacer')
-answers = ['1.Suma','2.Resta','3.Multiplicación','4.División','5.Módulo']
+# Variables relevantes
+"""Corresponde a qué operacion"""
+option: int
+"""Corresponde a cuántos numeros en la operación"""
+size: int
+"""Números de la operación"""
+numbers: list[int] = []
+
+# Seleccionar operación
+print('¿Qué quiere hacer?')
+answers = ['1. Suma','2. Resta','3. Multiplicación','4. División','5. Módulo']
 while True:
   print(answers)
   try:
@@ -22,39 +31,49 @@ while True:
   except ValueError:
     print(ANSWERS['INV'].format('No es un número'))
 
-number_1 = -1
-number_2 = -1
+# Seleccionar cantidad de números
+print('Efectuar operación sobre 2 o 3 números')
+answers = ['2. Dos números','3. Tres números']
 while True:
+  print(answers)
   try:
-    number_1 = int(input('Dé un número: '))
-    break
-  except ValueError:
-    print(ANSWERS['INV'].format('No es un número'))
-print('Dé otro número')
-while True:
-  try:
-    number_2 = int(input("Ingrese el otro número: "))
-    break
+    size = int(input("Ingresa el número 2 o 3: "))
+    if 2 <= size <= 3:
+      print(ANSWERS['ACCEPT'].format(answers[size-2]))
+      break
+    else:
+      print(ANSWERS['INV'].format('Número inválido'))
   except ValueError:
     print(ANSWERS['INV'].format('No es un número'))
 
+# Seleccionar los números
+INPUT_NUMBERS = ['Dé un un número: ', 'Dé otro número: ', 'Dé un último número: ']
+for s in range(0, size):
+  while True:
+    try:
+      numbers.append(int(input(INPUT_NUMBERS[s])))
+      break
+    except ValueError:
+      print(ANSWERS['INV'].format('No es un número'))
 
+# Resolver la operación con los números
+OPERATIONS = {
+  1: ['+', lambda a, b: a + b],
+  2: ['-', lambda a, b: a - b],
+  3: ['*', lambda a, b: a * b],
+  4: ['/', lambda a, b: a / b],
+  5: ['%', lambda a, b: a % b],
+}
 resultado = -1
-operacion = '+'
-if option == 1: resultado = number_1 + number_2
-elif option == 2:
-  resultado = number_1 - number_2
-  operacion = '-'
-elif option == 3:
-  resultado = number_1 * number_2
-  operacion = '*'
-elif option == 4:
-  operacion = '/'
-  try: resultado = number_1 / number_2
+if size == 2:
+  try: resultado = OPERATIONS[option][1](numbers[0], numbers[1])
   except ZeroDivisionError: print("No se puede dividir entre cero")
-elif option == 5:
-  operacion = '%'
-  try: resultado = number_1 % number_2
+else:
+  try: resultado = OPERATIONS[option][1](OPERATIONS[option][1](numbers[0], numbers[1]),numbers[2])
   except ZeroDivisionError: print("No se puede dividir entre cero")
 
-print(f'El resultado es: {number_1} {operacion} {number_2} = {resultado}')
+# Imprimir resultado
+last_print = 'El resultado es: '
+for i, number in enumerate(numbers):
+  last_print += f'{number} {OPERATIONS[option][0] if i + 1 < size else f"= {resultado}"}'
+print(last_print)
