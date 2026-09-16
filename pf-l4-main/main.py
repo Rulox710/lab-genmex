3186e6c6e2fbab637b33f0132d11913c7064813b
Raúl Nunova <rnunovaldes@ciencias.unam.mx>
2026-09-16 01:49:08 -0600

    Retirando while true y borrando archivo colado

--- pf-l4-main/main.py
+++ pf-l4-main/main.py
@@ -9,50 +9,58 @@ ANSWERS = {
 }
 
 # Variables relevantes
-"""Corresponde a qué operacion"""
 option: int
-"""Corresponde a cuántos numeros en la operación"""
+"""Corresponde a qué operacion"""
 size: int
-"""Números de la operación"""
+"""Corresponde a cuántos numeros en la operación"""
 numbers: list[int] = []
+"""Números de la operación"""
+
+in_loop: bool = True
+def switch_loop() -> bool:
+  global in_loop
+  in_loop = not(in_loop)
+  return not(in_loop)
 
 # Seleccionar operación
 print('¿Qué quiere hacer?')
 answers = ['1. Suma','2. Resta','3. Multiplicación','4. División','5. Módulo']
-while True:
+while in_loop:
   print(answers)
   try:
     option = int(input("Ingresa un número del 1 al 5: "))
     if 1 <= option <= 5:
       print(ANSWERS['ACCEPT'].format(answers[option-1]))
-      break
+      switch_loop()
     else:
       print(ANSWERS['INV'].format('Número inválido'))
   except ValueError:
     print(ANSWERS['INV'].format('No es un número'))
 
 # Seleccionar cantidad de números
+switch_loop()
 print('Efectuar operación sobre 2 o 3 números')
 answers = ['2. Dos números','3. Tres números']
-while True:
+while in_loop:
   print(answers)
   try:
     size = int(input("Ingresa el número 2 o 3: "))
     if 2 <= size <= 3:
       print(ANSWERS['ACCEPT'].format(answers[size-2]))
-      break
+      switch_loop()
     else:
       print(ANSWERS['INV'].format('Número inválido'))
   except ValueError:
     print(ANSWERS['INV'].format('No es un número'))
 
 # Seleccionar los números
+switch_loop()
 INPUT_NUMBERS = ['Dé un un número: ', 'Dé otro número: ', 'Dé un último número: ']
 for s in range(0, size):
-  while True:
+  while in_loop:
     try:
       numbers.append(int(input(INPUT_NUMBERS[s])))
-      break
+      switch_loop()
     except ValueError:
       print(ANSWERS['INV'].format('No es un número'))
 
